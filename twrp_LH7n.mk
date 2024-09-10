fe1ba4230f6bc2eacc0910559dddb11819daa5f1
Sora's Playground <kayozzprojects@yandex.com>
2024-09-10 22:12:10 +0800

    Create twrp_LH7n.mk
    Create a proper .mk file for lh7n

---
+++ twrp_LH7n.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from TECNO-LH7ndevice
+$(call inherit-product, device/tecno/TECNO-LH7n/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Product Specifics
+PRODUCT_NAME := twrp_LH7n
+PRODUCT_DEVICE := LH7n
+PRODUCT_BRAND := Tecno
+PRODUCT_MODEL := Tecno LH7n
+PRODUCT_MANUFACTURER := tecno
+
+PRODUCT_GMS_CLIENTID_BASE := android-transsion

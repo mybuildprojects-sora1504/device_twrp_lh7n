94744f89e78cc2a5faadf429ff00397899855511
Sora's Playground <kayozzprojects@yandex.com>
2024-09-10 22:16:56 +0800

    Create BoardConfig.mk

---
+++ BoardConfig.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/tecno/LH7n
+
+# Inherit from mt6789-common
+include device/transsion/mt6789-common/BoardConfigCommon.mk
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := Tecno-LH7n
+
+# TWRP Configs
+TW_DEVICE_VERSION := LH7n_by_rama982_sora1504
